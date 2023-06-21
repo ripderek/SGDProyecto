b0b80a26484a4114287d9d9f5f120a3401a9e83f
Raul <castillosteven09@outlook.es>
2023-06-20 21:55:50 -0500

    Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -15,4 +17,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+});
