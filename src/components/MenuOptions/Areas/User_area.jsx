657a220f1c43515c3682f84e73bb906e23a6fe8d
Raul <castillosteven09@outlook.es>
2023-07-02 22:21:50 -0500

    Consumir archivos

---
+++ src/components/MenuOptions/Areas/User_area.jsx
@@ -0,0 +1,37 @@
+import { React, useEffect, useState } from "react";
+import { Typography } from "@material-tailwind/react";
+export default function User_area(id_area) {
+  //consultar y almacenar los datos del area al que pertenece este usuario
+  const [data_area, setDataArea] = useState([]);
+  useEffect(() => {
+    load();
+  }, []);
+  const load = async () => {
+    try {
+      //almacenar datos del usuario con respecto al area para que ingrese a las opciones de usuario administrador de area
+      //y para mostrar opciones
+      const resultdata = await fetch(
+        "http://localhost:4000/api/area/data_area_id/" + id_area.id_area,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        }
+      );
+
+      const dataU = await resultdata.json();
+      setDataArea(dataU);
+    } catch (error) {
+      //Mostrar algun error por consola
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="bg-blue-gray-50 ">
+      <Typography variant="h1" className="ml-5">
+        {data_area.nombrearea}
+      </Typography>
+    </div>
+  );
+}
